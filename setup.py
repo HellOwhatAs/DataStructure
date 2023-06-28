951b39b2c50830ccb432452c4e6ed28c0dfbdfa1
HellOwhatAs <xjq701229@outlook.com>
2023-06-28 22:22:30 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="DataStructure",
+    author="HellOwhatAs",
+    url="https://github.com/HellOwhatAs/DataStructure",
+    packages=find_namespace_packages(),
+    zip_safe=False,
+)
