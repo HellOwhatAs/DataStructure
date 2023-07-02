29a1aad2c55293f05eefcda36425305d68256836
HellOwhatAs <xjq701229@outlook.com>
2023-07-02 12:33:40 +0800

    degree

--- DataStructure/graph/Graph.py
+++ DataStructure/graph/Graph.py
@@ -1,4 +1,4 @@
-from typing import Generator, Tuple
+from typing import Generator, Tuple, Literal
 from abc import ABC, abstractmethod
 from numbers import Real
 from math import inf, isinf
@@ -24,6 +24,9 @@ class Graph(ABC):
 
     @abstractmethod
     def get_neibours(self, node: int) -> Generator[Tuple[int, Real], None, None]:...
+
+    @abstractmethod
+    def degree(self, node: int, *args) -> int:...
 
 class AdjMatrixGraph(Graph):
     def __init__(self, num_nodes: int):
@@ -51,6 +54,10 @@ class AdjMatrixGraph(Graph):
             if not isinf(self.mat[node][idx]):
                 yield idx, self.mat[node][idx]
 
+    def degree(self, node: int) -> int:
+        return sum((not isinf(elem)) for elem in self.mat[node]) + (not isinf(self.mat[node][node]))
+        
+
 class DirectedAdjMatrixGraph(Graph):
     def __init__(self, num_nodes: int):
         self.n = num_nodes
@@ -76,6 +83,13 @@ class DirectedAdjMatrixGraph(Graph):
             if idx == node: continue
             if not isinf(self.mat[node][idx]):
                 yield idx, self.mat[node][idx]
+    
+    def degree(self, node: int, degree_type: Literal['in', 'out']) -> int:
+        if degree_type == 'in':
+            return sum((not isinf(self.mat[i][node])) for i in range(self.n))
+        elif degree_type == 'out':
+            return sum((not isinf(elem)) for elem in self.mat[node])
+        else: raise TypeError("Invalid degree_type")
 
 class AdjListGraph(Graph):
     def __init__(self, num_nodes: int):
@@ -102,6 +116,9 @@ class AdjListGraph(Graph):
         for k, v in self.list[node].items():
             yield k, v
 
+    def degree(self, node: int) -> int:
+        return len(self.list[node])
+
 class DirectedAdjListGraph(Graph):
     def __init__(self, num_nodes: int):
         self.n = num_nodes
@@ -125,3 +142,26 @@ class DirectedAdjListGraph(Graph):
     def get_neibours(self, node: int) -> Generator[Tuple[int, Real], None, None]:
         for k, v in self.list[node].items():
             yield k, v
+
+    def degree(self, node: int, degree_type: Literal['in', 'out']) -> int:
+        if degree_type == 'in':
+            return sum((node in l) for l in self.list)
+        elif degree_type == 'out':
+            return len(self.list[node])
+        else: raise TypeError("Invalid degree_type")
+
+if __name__ == '__main__':
+    g = DirectedAdjListGraph(7)
+    for edge in (
+        (1, 2),
+        (2, 3), (2, 4),
+        (3, 1),
+        (4, 1), (4, 3),
+        (5, 6), (5, 7),
+        (6, 2),
+        (7, 4), (7, 6)
+    ):
+        g.add_edge(edge[0]-1, edge[1]-1)
+
+    for i in range(len(g)):
+        print(i + 1, g.degree(i, 'in'), g.degree(i, 'out'))
