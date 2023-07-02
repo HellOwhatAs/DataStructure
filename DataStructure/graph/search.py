28fb81158f894e2a8a1e2ae5969cdd60fbb3b06e
HellOwhatAs <xjq701229@outlook.com>
2023-07-02 09:12:40 +0800

    bfs

--- DataStructure/graph/search.py
+++ DataStructure/graph/search.py
@@ -2,10 +2,12 @@ from typing import Callable, List, Optional
 
 try:
     from .Graph import Graph
+    from ..linear.Queue import Queue
 except ImportError:
     import sys, os
     sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
     from DataStructure.graph.Graph import Graph
+    from DataStructure.linear.Queue import Queue
     sys.path.pop()
     del sys, os
 
@@ -18,10 +20,33 @@ def __dfs(g: Graph, visited: List[bool], start: int, on_visit: Callable[[int], N
 
 def dfs(g: Graph, on_visit: Callable[[int, List[bool]], None], start: Optional[int] = None, *, on_tree_over: Optional[Callable[[List[bool]], None]] = None):
     visited = [False] * len(g)
-    if start is not None: __dfs(g, visited, start, on_visit)
+    if start is not None:
+        __dfs(g, visited, start, on_visit)
+        if on_tree_over is not None: on_tree_over(visited)
     for i in range(len(g)):
         if not visited[i]:
             __dfs(g, visited, i, on_visit)
+            if on_tree_over is not None: on_tree_over(visited)
+
+def __bfs(g: Graph, visited: List[bool], start: int, on_visit: Callable[[int], None]):
+    q = Queue()
+    q.push(start)
+    while not q.empty():
+        node = q.pop()
+        if visited[node]: continue
+        on_visit(node, visited)
+        visited[node] = True
+        for neibour, weight in g.get_neibours(node):
+            if not visited[neibour]: q.push(neibour)
+
+def bfs(g: Graph, on_visit: Callable[[int, List[bool]], None], start: Optional[int] = None, *, on_tree_over: Optional[Callable[[List[bool]], None]] = None):
+    visited = [False] * len(g)
+    if start is not None:
+        __bfs(g, visited, start, on_visit)
+        if on_tree_over is not None: on_tree_over(visited)
+    for i in range(len(g)):
+        if not visited[i]:
+            __bfs(g, visited, i, on_visit)
             if on_tree_over is not None: on_tree_over(visited)
 
 if __name__ == '__main__':
@@ -39,6 +64,5 @@ if __name__ == '__main__':
     ):
         g.add_edge(edge[0]-1, edge[1]-1)
 
-    dfs(g, lambda x, y: print(x + 1, end=' '), on_tree_over=print)
-
-    print(g.edge_weight(2, 0))
+    dfs(g, lambda x, y: print(x + 1, end=' '), start=4, on_tree_over=print)
+    bfs(g, lambda x, y: print(x + 1, end=' '), start=4, on_tree_over=print)
